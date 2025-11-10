aab00d67cf55963cda796062b6bd5fa6dc549e4f
TechnologicalJerry <cserajatverma@gmail.com>
2025-11-10 23:49:06 +0530

    Add asyncHandler utility for Express routes
    Introduces an asyncHandler function to simplify error handling in asynchronous Express route handlers by forwarding errors to the next middleware.

---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from 'express';
+
+export type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
+
+export const asyncHandler =
+  (handler: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(handler(req, res, next)).catch(next);
+  };
+
